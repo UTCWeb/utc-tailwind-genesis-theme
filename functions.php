e7652b0bd5cc9d02461a285320140f51e84b4427
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2024-02-06 15:25:07 -0500

    UTC-20240206: Update theme fonts +.

--- functions.php
+++ functions.php
@@ -114,8 +114,8 @@ function utc_enqueue_scripts_styles() {
 	);
 
     wp_enqueue_style( 
-		'google-roboto', 
-		'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', 
+		'google-raleway', 
+		'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap', 
 		false 
 	);
 
